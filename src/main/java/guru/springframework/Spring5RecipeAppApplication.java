2fce11da6613d59ff50488181edf6754d128ec7e
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-17 10:24:01 +0100

    Added findByDescription to Category and UOM repositories. Modified IndexController to include JPA queries for category and UOM.

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5RecipeAppApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("in main ###############################################");
         SpringApplication.run(Spring5RecipeAppApplication.class, args);
     }
 
--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Autowired
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        System.out.println("Id of American Category is " + categoryRepository.findByDescription("American").get().getId());
+        System.out.println("Id of Ounce UOM is " + unitOfMeasureRepository.findByDescription("Ounce").get().getId());
         return ("index");
     }
 }
